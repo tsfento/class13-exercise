6d04bc09fd4aa1503466e79913ddb030c6ca5186
Tyler Fenton <tsfento@gmail.com>
2023-10-07 10:31:03 -0500

    In progress

--- src/app/order-dashboard/order-dashboard.component.ts
+++ src/app/order-dashboard/order-dashboard.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./order-dashboard.component.css']
 })
 export class OrderDashboardComponent {
+  orders: string[] = [];
+  isWorking: boolean = false;
+  interval;
 
+  onBeginWorkday() {
+    if(this.isWorking) return;
+
+    this.isWorking = true;
+
+    let orderNumber: number = 1;
+
+    this.interval = setInterval(() => {
+      let order: string = 'Order number: ' + orderNumber.toString();
+
+      orderNumber++;
+      this.orders.push(order);
+      console.log(this.orders);
+    }, 2000);
+  }
+
+  onEndWorkday() {
+    if(!this.isWorking) return;
+
+    this.isWorking = false;
+    clearInterval(this.interval);
+  }
 }
